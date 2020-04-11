4ae2f7b56a5bf43b1ba28ef49a0469d819235883
imcasper <casper2756@gmail.com>
2020-04-11 13:49:20 +0300

    default project layout

--- babylon-helper/src/jsMain/kotlin/casper/util/TypesConverter.kt
+++ babylon-helper/src/jsMain/kotlin/casper/util/TypesConverter.kt
@@ -1,16 +1,54 @@
 package casper.util
 
 import BABYLON.*
+import casper.geometry.*
 import casper.geometry.Quaternion
-import casper.geometry.Vector3d
 import casper.geometry.aabb.AABBox3d
 import casper.geometry.polygon.Line3d
 import casper.math.clamp
 import casper.types.Color3d
 import casper.types.Color4d
+import kotlin.math.roundToInt
+
+
+fun Vector2i.toVector2(): Vector2 {
+	return Vector2(x.toDouble(), y.toDouble())
+}
+
+fun Vector2d.toVector2(): Vector2 {
+	return Vector2(this.x, this.y)
+}
+
+fun Vector2.toVector2d(): Vector2d {
+	return Vector2d(this.x, this.y)
+}
+
+fun Vector2.roundToVector2i(): Vector2i {
+	return Vector2i(x.roundToInt(), y.roundToInt())
+}
+
+fun Vector2.toVector2i(): Vector2i {
+	return Vector2i(x.toInt(), y.toInt())
+}
+
+fun Vector3i.toVector3(): Vector3 {
+	return Vector3(x.toDouble(), y.toDouble(), z.toDouble())
+}
 
 fun Vector3d.toVector3(): Vector3 {
 	return Vector3(this.x, this.y, this.z)
+}
+
+fun Vector3.toVector3d(): Vector3d {
+	return Vector3d(this.x, this.y, this.z)
+}
+
+fun Vector3.roundToVector3i(): Vector3i {
+	return Vector3i(x.roundToInt(), y.roundToInt(), z.roundToInt())
+}
+
+fun Vector3.toVector3i(): Vector3i {
+	return Vector3i(x.toInt(), y.toInt(), z.toInt())
 }
 
 fun Color3d.toColor3(): Color3 {
@@ -19,14 +57,6 @@ fun Color3d.toColor3(): Color3 {
 
 fun Color4d.toColor4(): Color4 {
 	return Color4(this.x, this.y, this.z, this.w)
-}
-
-fun Vector3.toVector3d(): Vector3d {
-	return Vector3d(this.x, this.y, this.z)
-}
-
-fun Vector3d.toView(): Vector3 {
-	return Vector3(this.x, this.z, -this.y)
 }
 
 fun Ray.toLine(maxLength: Double): Line3d {

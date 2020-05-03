e93e56c7fe7f1a6bb9ca5043e88d40a8b4425b52
imcasper <casper2756@gmail.com>
2020-05-03 14:09:36 +0300

    animation 0.9

--- example-experiment/src/main/kotlin/casper/app/Main.kt
+++ example-experiment/src/main/kotlin/casper/app/Main.kt
@@ -1,25 +1,28 @@
 package casper.app
 
 import casper.geometry.Vector3d
-import casper.render.Node
+import casper.render.Transform
+import casper.render.animation.Animations
 import casper.render.babylon.BabylonRenderEngine
-import casper.util.AssetManager
+import casper.render.node.Node
+import casper.util.AssetStorage
 
 
 fun main() {
 	val engine = BabylonRenderEngine.create("renderCanvas")
-	val assets = AssetManager(engine.nativeScene)
+	val assets = AssetStorage(engine.nativeScene)
 
 	createDefaultScene(engine.nativeScene)
 	assets.getSceneFuture("drill.babylon").thenAccept { sceneData ->
-		for (x in 0 until 64) {
-			for (y in 0 until 64) {
-				val offset = Vector3d(x.toDouble(), y.toDouble(), 0.0)
-				val nodes = sceneData.nodeList.map { it.move(offset) }
-
-				nodes.forEach {
-					engine.addNode(it)
-				}
+		for (x in 0 until 8) {
+			for (y in 0 until 8) {
+				engine.addNode(
+						Node(
+								Transform(position = Vector3d(x.toDouble(), y.toDouble(), 0.0)),
+								sceneData.content,
+								Animations(true, (x / 64.0 + y), emptyList())
+						)
+				)
 			}
 		}
 

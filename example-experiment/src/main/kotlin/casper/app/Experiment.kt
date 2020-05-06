d4b4d81754ff897bbdc94f473d65971089950608
imcasper <casper2756@gmail.com>
2020-05-06 17:37:31 +0300

    .

--- example-experiment/src/main/kotlin/casper/app/Experiment.kt
+++ example-experiment/src/main/kotlin/casper/app/Experiment.kt
@@ -1,5 +1,6 @@
 package casper.app
 
+import casper.collection.map.MapUtil
 import casper.collection.nextItem
 import casper.geometry.Vector2d
 import casper.geometry.Vector3d
@@ -7,12 +8,8 @@ import casper.geometry.basis.Box2d
 import casper.geometry.polygon.Quad
 import casper.render.Environment
 import casper.render.ModelReference
-import casper.render.Render
 import casper.render.babylon.BabylonRender
-import casper.render.material.FloatConstantReference
-import casper.render.material.Material
-import casper.render.material.MaterialReference
-import casper.render.material.TextureReference
+import casper.render.material.*
 import casper.render.node.Content
 import casper.render.node.Node
 import casper.render.vertex.Vertex
@@ -25,7 +22,9 @@ import casper.util.atlas.Atlas
 import casper.util.createCubeTextureFromPlane
 import kotlin.random.Random
 
-fun createTiles( size: Int, pivot: Vector3d, regions: List<Box2d>):Vertices {
+data class TileInfo(val albedo:Box2d, val special:Box2d)
+
+fun createTiles(size: Int, pivot: Vector3d, tiles: List<TileInfo>): Vertices {
 
 	val random = Random(pivot.hashCode())
 
@@ -33,14 +32,16 @@ fun createTiles( size: Int, pivot: Vector3d, regions: List<Box2d>):Vertices {
 
 	for (x in 0 until size) {
 		for (y in 0 until size) {
-			val region = random.nextItem(regions) ?: continue
+			val tile = random.nextItem(tiles) ?: continue
 
 			val relativePivot = Vector3d(x.toDouble(), y.toDouble(), 0.0)
+			val albedo = tile.albedo
+			val special = tile.special
 			builder.add(Quad(
-					Vertex(relativePivot + pivot + Vector3d(0.0, 1.0, 0.0), uv = Vector2d(region.min.x, region.max.y)),
-					Vertex(relativePivot + pivot + Vector3d(1.0, 1.0, 0.0), uv = Vector2d(region.max.x, region.max.y)),
-					Vertex(relativePivot + pivot + Vector3d(1.0, 0.0, 0.0), uv = Vector2d(region.max.x, region.min.y)),
-					Vertex(relativePivot + pivot + Vector3d(0.0, 0.0, 0.0), uv = Vector2d(region.min.x, region.min.y))
+					Vertex(relativePivot + pivot + Vector3d(0.0, 1.0, 0.0), uv = Vector2d(albedo.min.x, albedo.max.y),uv2 = Vector2d(special.min.x, special.max.y)),
+					Vertex(relativePivot + pivot + Vector3d(1.0, 1.0, 0.0), uv = Vector2d(albedo.max.x, albedo.max.y),uv2 = Vector2d(special.max.x, special.max.y)),
+					Vertex(relativePivot + pivot + Vector3d(1.0, 0.0, 0.0), uv = Vector2d(albedo.max.x, albedo.min.y),uv2 = Vector2d(special.max.x, special.min.y)),
+					Vertex(relativePivot + pivot + Vector3d(0.0, 0.0, 0.0), uv = Vector2d(albedo.min.x, albedo.min.y),uv2 = Vector2d(special.min.x, special.min.y))
 			))
 		}
 	}
@@ -65,47 +66,43 @@ fun main() {
 
 	createDefaultScene(render.nativeScene)
 
-	assets.getAtlasFuture("albedo.atlas").thenAccept { atlas ->
-		assets.getBitmapFuture("rock.png").thenAccept { rockBitmap ->
-			assets.getBitmapFuture("sand.png").thenAccept { sandBitmap ->
-				assets.getBitmapFuture("soil.png").thenAccept { soilBitmap ->
-					assets.getBitmapFuture("water.png").thenAccept { waterBitmap ->
-						assets.getBitmapFuture("skybox_hdr.png").thenAccept { skyBoxBitmap ->
-							assets.getBitmapFuture("template.png").thenAccept { templateBitmap ->
+	assets.getAtlasFuture("albedo.atlas").thenAccept { albedoAtlas ->
+		assets.getAtlasFuture("special.atlas").thenAccept { specialAtlas ->
+			assets.getBitmapFuture("skybox_hdr.png").thenAccept { skyBoxBitmap ->
+				assets.getBitmapFuture("template.png").thenAccept { templateBitmap ->
 
-								val skyboxTexture = createCubeTextureFromPlane(skyBoxBitmap, "skybox")
-								render.environment = Environment(
-										Color4d(0.1, 0.5, 0.8, 1.0),
-										skyboxTexture,
-										true
-								)
-								val page = atlas.pages.values.first()
+					val skyboxTexture = createCubeTextureFromPlane(skyBoxBitmap, "skybox")
+					render.environment = Environment(
+							Color4d(0.1, 0.5, 0.8, 1.0),
+							skyboxTexture,
+							true
+					)
+					val page = albedoAtlas.pages.values.first()
+					val special =  TextureReference(page.bitmap, "atlas-special")
+					val roughness = MapUtil.takeChannel(special.data, 1)
+					val metallic = MapUtil.takeChannel(special.data, 2)
+					val material = MaterialReference(name = "atlas", data = Material(albedo = TextureReference(page.bitmap, "atlas-albedo"), roughness = FloatMapReference(roughness), metallic = FloatMapReference(metallic)))
 
-								val material = MaterialReference(name = "atlas", data = Material(albedo = TextureReference(page.bitmap, "atlas"), roughness = FloatConstantReference(0.8), metallic = FloatConstantReference(0.0)))
+					val tiles = listOf(
+							TileInfo(albedoAtlas.getTextureRegion("rock")!!,specialAtlas.getTextureRegion("rock-m")!!),
+							TileInfo(albedoAtlas.getTextureRegion("sand")!!,specialAtlas.getTextureRegion("sand-m")!!),
+							TileInfo(albedoAtlas.getTextureRegion("soil")!!,specialAtlas.getTextureRegion("soil-m")!!),
+							TileInfo(albedoAtlas.getTextureRegion("water")!!,specialAtlas.getTextureRegion("water-m")!!)
+					)
 
-								val regions = listOf(
-										atlas.getTextureRegion("rock")!!,
-										atlas.getTextureRegion("sand")!!,
-										atlas.getTextureRegion("soil")!!,
-										atlas.getTextureRegion("water")!!
-								)
+					assets.getSceneFuture("drill.babylon").thenAccept { sceneData ->
+						createDrills(render, sceneData, skyboxTexture)
+						createAnimatedCube(render, templateBitmap)
 
-								assets.getSceneFuture("drill.babylon").thenAccept { sceneData ->
-									createDrills(render, sceneData, skyboxTexture)
-									createAnimatedCube(render, templateBitmap)
-
-									for (s in 0 until 8) {
-										for (t in 0 until 8) {
-											val size = 8
-											val vertices = createTiles( size, Vector3d((s * size).toDouble(), (t * size).toDouble(), 0.0), regions)
-											render.addNode(Node(content = Content(ModelReference(VerticesReference(vertices), material), name = "tile")))
-										}
-									}
-								}
-
+						for (s in 0 until 8) {
+							for (t in 0 until 8) {
+								val size = 8
+								val vertices = createTiles(size, Vector3d((s * size).toDouble(), (t * size).toDouble(), 0.0), tiles)
+								render.addNode(Node(content = Content(ModelReference(VerticesReference(vertices), material), name = "tile")))
 							}
 						}
 					}
+
 				}
 			}
 		}

6f5c99c04c5de4a83e27e416a180bfeb38e15d69
imcasper <casper2756@gmail.com>
2020-05-10 14:55:50 +0300

    .

--- example-experiment/src/main/kotlin/casper/app/Drills.kt
+++ example-experiment/src/main/kotlin/casper/app/Drills.kt
@@ -8,6 +8,7 @@ import casper.render.SceneData
 import casper.render.extension.MaterialReplacer
 import casper.render.material.ColorConstantReference
 import casper.render.material.CubeTextureReference
+import casper.render.material.Material
 import casper.render.material.MaterialReference
 import casper.render.model.ModelTransform
 import casper.render.model.TimeLine
@@ -31,13 +32,18 @@ fun createDrills(render: Render, sceneData: SceneData, skyboxTexture: CubeTextur
 		} else null
 	}
 
+	val wireFrameModel = MaterialReplacer.execute(sceneData.model, "WireFrame-drill") {
+		MaterialReference(Material(wireFrame = true))
+	}
+
 
 	for (x in 0 until 8) {
 		for (y in 0 until 8) {
+			val wireframe = x == 7 - y
 			render.addChild(ModelTransform(
-					Transform(position = Vector3d(x.toDouble() * 4.0, y.toDouble() * 4.0, 0.5), scale = Vector3d.ONE, rotation = Quaternion.IDENTITY),
-					if (y == x) blueModel else redModel,
-					TimeLine(speed = y * (x / 8.0 + 1.0))
+					Transform(position = Vector3d(x.toDouble() * 2.0, y.toDouble() * 2.0, 0.5), scale = Vector3d.ONE, rotation = Quaternion.IDENTITY),
+					if (wireframe) wireFrameModel else if (y == x) blueModel else redModel,
+					TimeLine(speed = (y + 1.0 / 8.0) * (x / 8.0 + 1.0 / 8.0))
 			))
 		}
 	}

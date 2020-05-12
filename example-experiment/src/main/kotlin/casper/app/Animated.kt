39b8b30948a7b69d48a86dedbfdaa4cb5660148a
imcasper <casper2756@gmail.com>
2020-05-12 20:20:44 +0300

    special texture animation

--- example-experiment/src/main/kotlin/casper/app/Animated.kt
+++ example-experiment/src/main/kotlin/casper/app/Animated.kt
@@ -1,5 +1,6 @@
 package casper.app
 
+import casper.collection.map.MapUtil
 import casper.geometry.Matrix4d
 import casper.geometry.Vector2d
 import casper.geometry.Vector3d
@@ -7,9 +8,7 @@ import casper.geometry.polygon.Octagon
 import casper.render.Render
 import casper.render.extension.TextureUVAnimator
 import casper.render.extension.VerticesBuilder
-import casper.render.material.FloatConstantReference
-import casper.render.material.Material
-import casper.render.material.TextureReference
+import casper.render.material.*
 import casper.render.model.SceneModel
 import casper.render.model.SceneNode
 import casper.render.vertex.Vertex
@@ -19,23 +18,26 @@ import casper.types.Bitmap
 fun createAnimatedCube(render: Render, templateBitmap: Bitmap) {
 	val list = VerticesBuilder().add(
 			Octagon(
-					Vertex(uvAlbedo = Vector2d(0.0, 0.0), position = Vector3d(0.0, 0.0, 0.0)),
-					Vertex(uvAlbedo = Vector2d(0.0, 1.0), position = Vector3d(0.0, 2.0, 0.0)),
-					Vertex(uvAlbedo = Vector2d(1.0, 0.0), position = Vector3d(2.0, 0.0, 0.0)),
-					Vertex(uvAlbedo = Vector2d(1.0, 1.0), position = Vector3d(2.0, 2.0, 0.0)),
-
-					Vertex(uvAlbedo = Vector2d(1.0, 1.0), position = Vector3d(0.0, 0.0, 1.0)),
-					Vertex(uvAlbedo = Vector2d(1.0, 0.0), position = Vector3d(0.0, 2.0, 1.0)),
-					Vertex(uvAlbedo = Vector2d(0.0, 1.0), position = Vector3d(2.0, 0.0, 1.0)),
-					Vertex(uvAlbedo = Vector2d(0.0, 0.0), position = Vector3d(2.0, 2.0, 1.0))
+					Vertex(uvAlbedo = Vector2d(0.0, 0.0), uvMetallic = Vector2d(0.0, 0.0), position = Vector3d(0.0, 0.0, 0.0)),
+					Vertex(uvAlbedo = Vector2d(0.0, 1.0), uvMetallic = Vector2d(0.0, 1.0), position = Vector3d(0.0, 2.0, 0.0)),
+					Vertex(uvAlbedo = Vector2d(1.0, 0.0), uvMetallic = Vector2d(1.0, 0.0), position = Vector3d(2.0, 0.0, 0.0)),
+					Vertex(uvAlbedo = Vector2d(1.0, 1.0), uvMetallic = Vector2d(1.0, 1.0), position = Vector3d(2.0, 2.0, 0.0)),
+					Vertex(uvAlbedo = Vector2d(1.0, 1.0), uvMetallic = Vector2d(1.0, 1.0), position = Vector3d(0.0, 0.0, 1.0)),
+					Vertex(uvAlbedo = Vector2d(1.0, 0.0), uvMetallic = Vector2d(1.0, 0.0), position = Vector3d(0.0, 2.0, 1.0)),
+					Vertex(uvAlbedo = Vector2d(0.0, 1.0), uvMetallic = Vector2d(0.0, 1.0), position = Vector3d(2.0, 0.0, 1.0)),
+					Vertex(uvAlbedo = Vector2d(0.0, 0.0), uvMetallic = Vector2d(0.0, 0.0), position = Vector3d(2.0, 2.0, 1.0))
 			))
-	val transform = Matrix4d.translate(Vector3d(0.5, 0.5, 0.0))
+	val matrix = Matrix4d.translate(Vector3d(0.5, 0.5, 0.0))
 
-	val albedoAnimated = TextureReference(templateBitmap, "animated", transform)
-	val material = Material(albedo = albedoAnimated, roughness = FloatConstantReference(0.95), metallic = FloatConstantReference(0.0))
+	val albedoAnimated = TextureReference(templateBitmap, "animated", TextureTransform(matrix = matrix))
+	val specialAnimated = TextureReference(templateBitmap, "animated", TextureTransform(matrix = matrix))
+
+	val specialChannel = FloatMapReference(MapUtil.takeChannel(specialAnimated.data, 0), null, specialAnimated.transform)
+
+	val material = Material(albedo = albedoAnimated, roughness = FloatConstantReference(0.95), metallic = specialChannel)
 	render.addChild(SceneNode(model = SceneModel(list, material)))
 
-	TextureUVAnimator(render, albedoAnimated, Vector3d(1.0, 0.0, 0.0))
+	TextureUVAnimator(render, specialChannel.transform, Vector3d(1.0, 0.0, 0.0))
 
 
 }

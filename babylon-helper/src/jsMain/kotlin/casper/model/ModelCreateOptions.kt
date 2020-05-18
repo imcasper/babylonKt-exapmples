212fb1be15cdc2c146845ef50301787c333ed351
imcasper <casper2756@gmail.com>
2020-05-18 19:56:39 +0300

    some const to int

--- babylon-helper/src/jsMain/kotlin/casper/model/ModelCreateOptions.kt
+++ babylon-helper/src/jsMain/kotlin/casper/model/ModelCreateOptions.kt
@@ -1,3 +1,3 @@
 package casper.model
 
-class ModelCreateOptions(val ignoreCameras: Boolean = true, val ignoresLights: Boolean = true, val isPickable: Boolean? = null, val cullingStrategy: Double? = null, val castShadow: Boolean = true, val receiveShadow: Boolean = true)
+class ModelCreateOptions(val ignoreCameras: Boolean = true, val ignoresLights: Boolean = true, val isPickable: Boolean? = null, val cullingStrategy: Int? = null, val castShadow: Boolean = true, val receiveShadow: Boolean = true)
